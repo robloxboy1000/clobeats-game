02d2283a929b98c562f6a6e47c86525f78a4d22f
PixlPlaya5 <pixlplaya5@gmail.com>
2026-01-14 13:48:54 -0600

    01/14/2026 fixed prefabs, using ManagedBass for audio, note synchronization broken

---
+++ Assets/Scripts/ScheduledTime.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ScheduledTime : MonoBehaviour
+{
+    // scheduled song time in seconds when this object should be at the strike line
+    public float scheduledSeconds = 0f;
+}
